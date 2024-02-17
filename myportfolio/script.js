b603e8479a1d0f794a668092992afeaa309f0e2e
SujithaNagasuri <sujithanagasuri5607@gmail.com>
2024-02-17 23:39:16 +0530

    education sections is added

---
+++ myportfolio/script.js
@@ -0,0 +1,10 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.accordion-item,.about-intro', { origin: 'right' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.heading', { origin: 'top' });
