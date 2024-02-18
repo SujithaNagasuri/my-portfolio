3c6686baca34fdf2f8beb4de50172c4629d92a63
SujithaNagasuri <sujithanagasuri5607@gmail.com>
2024-02-18 22:55:27 +0530

    certification section addded

--- myportfolio/script.js
+++ myportfolio/script.js
@@ -8,3 +8,13 @@ ScrollReveal({
 ScrollReveal().reveal('.accordion-item,.about-intro,.right', { origin: 'right' });
 ScrollReveal().reveal('.about-img,.quote, .left', { origin: 'left' });
 ScrollReveal().reveal('.heading', { origin: 'top' });
+
+
+
+var element = document.querySelectorAll('.nav-item');
+element.forEach(ele =>{
+    ele.onclick =()=>{
+        element.forEach(el => el.classList.remove('linkcolor'));
+        ele.classList.toggle('linkcolor');
+    }
+})
